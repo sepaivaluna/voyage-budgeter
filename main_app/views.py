9737616dea8f82aee647d20e4c42a5b6e06e0132
sepaivaluna <sepaivaluna@gmail.com>
2021-04-30 19:06:11 -0400

    fixed bug

--- main_app/views.py
+++ main_app/views.py
@@ -19,7 +19,7 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('index')
+      return redirect('budgets_index')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
