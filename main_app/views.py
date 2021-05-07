75ab4cb2721c4fa3ab433b2b2cdf1a9968421c21
sepaivaluna <sepaivaluna@gmail.com>
2021-05-06 23:11:59 -0400

    added extra styling

--- main_app/views.py
+++ main_app/views.py
@@ -34,7 +34,7 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('budgets_index')
+      return redirect('home')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
