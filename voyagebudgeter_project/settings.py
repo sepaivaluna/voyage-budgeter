e048f5dffd4a25ef13eca40edf2e3bc1227a476a
sepaivaluna <sepaivaluna@gmail.com>
2021-05-10 15:53:43 -0400

    removed forward slash from domain

--- voyagebudgeter_project/settings.py
+++ voyagebudgeter_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-# import django_on_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://voyagebudgeter.herokuapp.com/']
+ALLOWED_HOSTS = ['https://voyagebudgeter.herokuapp.com']
 
 
 # Application definition
@@ -131,4 +131,4 @@ LOGOUT_REDIRECT_URL = '/accounts/login'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# django_on_heroku.settings(locals())
+django_on_heroku.settings(locals())
